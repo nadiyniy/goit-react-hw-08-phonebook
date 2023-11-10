9313d91510038c87be933c4c6bbb644b84cee504
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-10 20:35:24 +0200

    fix

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarLoader, BeatLoader } from 'react-spinners';
+import { BarLoader } from 'react-spinners';
 import styled from 'styled-components';
 
 const Loader = () => {
